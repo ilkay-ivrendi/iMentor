f86f7c45de2fd56fc683497cda6df4e464e05db7
ilkay-ivrendi <ilkay.ivrendi@gmail.com>
2025-04-29 23:46:35 +0300

    basic chat service

---
+++ src/app/core/services/chat.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface ChatMessage {
+  sender: string;
+  receiver: string;
+  message: string;
+  timestamp?: Date;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  private apiUrl = 'http://localhost:8080/api/v1/chat';
+
+  constructor(private http: HttpClient) {}
+
+  // Send a message
+  sendMessage(chatMessage: ChatMessage): Observable<any> {
+    return this.http.post(`${this.apiUrl}/send`, chatMessage);
+  }
+
+  // Get chat history between two users
+  getMessages(sender: string, receiver: string): Observable<ChatMessage[]> {
+    return this.http.get<ChatMessage[]>(
+      `${this.apiUrl}/history?sender=${sender}&receiver=${receiver}`
+    );
+  }
+
+  // Optionally: Get all conversations of current user
+  getConversations(username: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/conversations/${username}`);
+  }
+}
